3fdd55d94136731a3578f38c4cc1a4b45e50d6d4
dulcetiseduce1 <mariusdulce@tuta.io>
2022-06-03 22:07:43 +0200

    Revert "attempt"
    This reverts commit 9151bc30f2430c7a6b01db2fa0cf76b6681db6d0.

--- js/main.js
+++ js/main.js
@@ -18,37 +18,21 @@ console.log(numerilog);
 
 //avviare un timer di 30 secondi
 setTimeout(timer, 3000);
-//dopo 30 secondi nascondere i numeri
+
 function timer() {
   numberList.classList.add("d-none");
 }
 
+//dopo 30 secondi nascondere i numerifunction
 setTimeout(inserisciNumeri, 3500);
 function inserisciNumeri(){
 let numeriUtente = [];
 while (numeriUtente.length < 5) {
-  let answer = prompt("Inserisci i numeri uno alla volta.");
-  let promptNumber = parseInt(answer);
+  let prompt = prompt("Inserisci i numeri uno alla volta.");
+  let promptNumber = parseInt(prompt);
   numeriUtente.push(promptNumber);
 }
 console.log(numeriUtente);
 }
-
-let numeriIndovinati = [];
-function controllo() {
-    for (i = 0; i < 5; i++) {
-      if (numeriArray.includes(numeriUtente[i])) {
-        numeriIndovinati.push(numeriUtente[i]);
-      }
-    }
-  
-    console.log(numeriIndovinati);
-    alert(
-      "Hai ricordato " +
-        numeriIndovinati.length +
-        "numeri e sono i seguenti: " +
-        numeriIndovinati
-    );
-  }
 //prompt utente inserisci numeri
 //5 numeri inseriti dice quali giusti
